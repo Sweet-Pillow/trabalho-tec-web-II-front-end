6cc54f90d9da686a3b05114ba970bc1e6f8a1629
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-08-04 16:51:06 -0300

    feat: add initial version

--- src/components/Answer/index.jsx
+++ src/components/Answer/index.jsx
@@ -1,26 +1,28 @@
-import Vote from '../Vote/index'
+import PropTypes from "prop-types";
+import Vote from "../Vote/index";
 
-export default function(props) {
-	return(
-		<div className="flex flex-row border-t border-gray-300 py-4 mb-4">
-				<Vote votes={props.votes} />
-
-				<div className="px-10 flex flex-col gap-y-6">
-
-					<div className="flex flex-row gap-5">
-						<div className="flex flex-row text-xs">
-							<p className="text-gray-700">Respondida por:&nbsp;</p>
-							<p>{props.userName}</p>
-						</div>
-						<div className="flex flex-row text-xs">
-							<p className="text-gray-700">Criada em:&nbsp;</p>
-							<p>{props.createData}</p>
-						</div>
-					</div>
-
-					<p className="text-lg">{props.answer}</p>
-				</div>
-
-			</div>
-	)
+function Answer({ votes, userName, createData, answer }) {
+  return (
+    <div className="flex flex-row border-t border-gray-200 px-6 py-8 mb-8 bg-gray-50 rounded-lg">
+      <Vote votes={votes} />
+      <div className="px-4 flex flex-col flex-grow gap-y-2">
+        <div className="flex flex-row justify-between text-sm text-gray-600 mb-2">
+          <p>
+            Respondida por: <span className="font-semibold">{userName}</span>
+          </p>
+          <p>Criada em: {createData}</p>
+        </div>
+        <p className="text-base">{answer}</p>
+      </div>
+    </div>
+  );
 }
+
+Answer.propTypes = {
+  votes: PropTypes.number.isRequired,
+  userName: PropTypes.string.isRequired,
+  createData: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired,
+};
+
+export default Answer;
--- src/pages/Question/index.jsx
+++ src/pages/Question/index.jsx
@@ -1,25 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Vote from "../../components/Vote/index";
 import Tag from "../../components/Tag/index";
 import Answer from "../../components/Answer/index";
 import api from "../../lib/api";
-import Cookies from "js-cookie";
 
 export default function QuestionDetail() {
-  const { id } = useParams(); // Obtém o ID da pergunta da URL
+  const { id } = useParams();
   const [question, setQuestion] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  console.log(id);
-
   useEffect(() => {
-    // Faz a requisição para obter a pergunta e suas respostas
     api
       .get(`/questions/${id}`)
       .then((response) => {
-        setQuestion(response.data); // Armazena a pergunta no estado
+        setQuestion(response.data);
         setLoading(false);
       })
       .catch((error) => {
@@ -27,7 +22,7 @@ export default function QuestionDetail() {
         setError(error);
         setLoading(false);
       });
-  }, [id]); // O efeito será reexecutado quando o ID mudar
+  }, [id]);
 
   if (loading) return <p>Carregando...</p>;
   if (error) return <p>Erro ao carregar a pergunta.</p>;
@@ -36,44 +31,31 @@ export default function QuestionDetail() {
   const { title, content, tags, createdAt, author, answers } = question;
 
   return (
-    <div className="w-full px-10 flex flex-col">
-      {/* Início seção da pergunta */}
-      <div className="py-9 flex flex-col justify-start gap-2 border-b border-gray-300">
-        <h1 className="text-[2rem]">{title}</h1>
-
-        <div className="flex flex-row gap-5">
-          <div className="flex flex-row text-xs">
-            <p className="text-gray-700">Perguntado por:&nbsp;</p>
-            <p>{author?.name || "Desconhecido"}</p>
-          </div>
-          <div className="flex flex-row text-xs">
-            <p className="text-gray-700">Criado em:&nbsp;</p>
-            <p>{new Date(createdAt).toLocaleDateString()}</p>
-          </div>
+    <div className="max-w-[900px] px-6 py-8 md:px-10 flex flex-col">
+      {/* Seção da pergunta */}
+      <div className="px-6 py-8 md:py-8 flex flex-col gap-4  border-b border-gray-200 bg-gray-100 rounded-lg mb-6">
+        <h1 className="text-2xl md:text-3xl font-bold">{title}</h1>
+        <div className="flex flex-row justify-between text-sm text-gray-600 mb-4">
+          <p>
+            Perguntado por:{" "}
+            <span className="font-semibold">
+              {author?.name || "Desconhecido"}
+            </span>
+          </p>
+          <p>Criado em: {new Date(createdAt).toLocaleDateString()}</p>
+        </div>
+        <p className="text-base md:text-lg">{content}</p>
+        <div className="flex flex-wrap gap-2 mt-2">
+          {(tags || "").split(",").map((tagName, index) => (
+            <Tag key={index} title={tagName} />
+          ))}
         </div>
       </div>
 
-      <div className="flex flex-row py-4">
-        <Vote votes={question.votes || 0} />
-
-        <div className="px-10 flex flex-col gap-y-6">
-          <p className="text-lg">{content}</p>
-          <div className="flex flex-row gap-x-2">
-            {(tags || "").split(",").map((tagName, index) => (
-              <Tag key={index} title={tagName} />
-            ))}
-          </div>
-        </div>
+      {/* Seção de respostas */}
+      <div className="text-lg font-semibold mb-4">
+        <p>{answers.length} Resposta(s)</p>
       </div>
-
-      {/* Fim seção da pergunta */}
-
-      <div className="flex flex-row mt-6 mb-3 text-[1.3rem]">
-        <p>{answers.length}&nbsp;</p>
-        <p> Resposta(s)</p>
-      </div>
-
-      {/* Início seção de visualizar respostas */}
 
       {answers.map((answer) => (
         <Answer
@@ -85,29 +67,25 @@ export default function QuestionDetail() {
         />
       ))}
 
-      {/* Fim seção de visualizar respostas */}
-
-      {/* Início seção de postar resposta */}
-
-      <div className="flex flex-col gap-1 border-t border-gray-300 mb-4 mt-8">
-        <label htmlFor="answer" className="text-[1.3rem] py-4">
+      {/* Seção de postar resposta */}
+      <div className="flex flex-col gap-2 border-t border-gray-200 pt-4 mt-6">
+        <label htmlFor="answer" className="text-lg font-semibold mb-2">
           Escrever uma resposta
         </label>
         <textarea
           name="answer"
           rows={4}
           cols={1}
-          className="resize-none focus:border-blue-800 focus:outline-none focus:ring-1 border border-gray-400 rounded-lg w-full placeholder:text-sm placeholder:px-1 py-1"
+          className="resize-none border border-gray-300 rounded-lg w-full px-2 py-1 text-base focus:border-blue-500 focus:ring-1 placeholder:text-sm"
+          placeholder="Escreva sua resposta aqui..."
         ></textarea>
         <button
           type="button"
-          className="mt-2 bg-blue-600 text-white rounded px-2 py-2 w-fit h-fit hover:bg-blue-700"
+          className="mt-2 bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700"
         >
           Responder
         </button>
       </div>
-
-      {/* Fim seção de postar resposta */}
     </div>
   );
 }
